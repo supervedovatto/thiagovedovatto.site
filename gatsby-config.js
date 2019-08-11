505c34c784d7c60830dadc3105fce9ae49c719b2
supervedovatto <supervedovatto@hotmail.com>
2019-08-10 21:53:46 -0300

    Implementação do Google Analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -8,7 +8,7 @@ module.exports = {
   plugins: [
     'gatsby-plugin-react-helmet',
     {
-      resolve: `gatsby-plugin-manifest`,
+    resolve: `gatsby-plugin-manifest`,
       options: {
         name: config.manifestName,
         short_name: config.manifestShortName,
@@ -21,5 +21,44 @@ module.exports = {
     },
     'gatsby-plugin-sass',
     'gatsby-plugin-offline',
+    {
+     resolve: `gatsby-plugin-google-analytics`,
+        options: {
+        trackingId: "145399492",
+        // Defines where to place the tracking script - `true` in the head and `false` in the body
+        head: false,
+        // Setting this parameter is optional
+        anonymize: true,
+        // Setting this parameter is also optional
+        respectDNT: true,
+        // Avoids sending pageview hits from custom paths
+        exclude: ["/preview/**", "/do-not-track/me/too/"],
+        // Delays sending pageview hits on route update (in milliseconds)
+        pageTransitionDelay: 0,
+        // Enables Google Optimize using your container Id
+        optimizeId: "UA-145399492-1",
+        // Enables Google Optimize Experiment ID
+        // experimentId: "YOUR_GOOGLE_EXPERIMENT_ID",
+        // Set Variation ID. 0 for original 1,2,3....
+        // variationId: "YOUR_GOOGLE_OPTIMIZE_VARIATION_ID",
+        // Any additional optional fields
+        sampleRate: 5,
+        siteSpeedSampleRate: 10,
+        // cookieDomain: "thiagovedovatto.site",
+      },
+    },
   ],
 };
+
+// import React
+// import { OutboundLink } from 'gatsby-plugin-google-analytics'
+
+// export default () => {
+//   <div>
+//     <OutboundLink
+//       href="https://www.gatsbyjs.org/packages/gatsby-plugin-google-analytics/"
+//     >
+//       Visit the Google Analytics plugin page!
+//     </OutboundLink>
+//   </div>
+// };

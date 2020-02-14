6b8b82454541204b5849d955f5edcfa6f4732681
supervedovatto <supervedovatto@hotmail.com>
2020-02-14 12:27:07 -0300

    Inclusão do script para instalação dos pacotes e dependências do gatsby

---
+++ mysite_gatsby_framework.sh
@@ -0,0 +1,22 @@
+#Instala todas as bibliotecas que preciso para o gatsby framework
+cd ~/Documents/MySite
+npm install -g npm
+npm install -g gatsby-cli
+npm install --save core-js
+npm install --save @gatsbyjs/relay-compiler
+npm install --save fsevents@*
+npm install --save typescript@*
+npm install --save @babel/core
+npm install --save babel-preset-gatsby
+npm install --save gatsby-source-graphql
+npm install --save typography 
+npm install --save gatsby-plugin-typography 
+npm install --save react-typography 
+npm install --save typography-theme-fairy-gates
+npm install --save typography-theme-kirkham 
+npm install --save gatsby-transformer-remark
+npm install --save @emotion/core
+npm install --save gatsby-plugin-emotion
+npm install --save gatsby-source-filesystem
+npm install --save katex
+npm install --save gatsby-remark-katex
